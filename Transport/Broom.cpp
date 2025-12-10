2f56b060094959f047b16ef245affaa9df380d01
Sergei <netotest@neto.com>
2025-12-10 18:02:51 +0600

    Small correction

--- Transport/Broom.cpp
+++ Transport/Broom.cpp
@@ -7,7 +7,11 @@ void Broom::RacingTime(int distance)
     double newDistance{};
 
     DSR = distance / 1000;
-
-    newDistance = (1.0 - (static_cast<double>(DSR) / 100.0)) * static_cast<double>(distance);
-    resultTime = newDistance / static_cast<double>(speed);
+    if (DSR >= 1000)
+        resultTime = 0;
+    else
+    {
+        newDistance = (1.0 - (static_cast<double>(DSR) / 100.0)) * static_cast<double>(distance);
+        resultTime = newDistance / static_cast<double>(speed);
+    }
 }
